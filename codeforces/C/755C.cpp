cc7b31c8d856b27d51084f5758fa2094424e79f3
Parth Gupta <51705666+parthgpta@users.noreply.github.com>
2019-10-10 18:34:46 +0530

    Deleted Comments on Solution for 755C Codeforces (#59)
    * 755C
    
    * Added Solutions
    
    * Updated 755C

---
+++ codeforces/C/755C.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<int, vector<int> > mp;
+vector< bool> b(10000,0);
+vector<bool>visited(100000,0);
+
+void dfs(long int n){
+	visited[n]=true;
+	
+	for(int i=0;i<mp[n].size();i++){
+		if(!visited[mp[n][i]])
+			dfs(mp[n][i]);
+	}
+}
+
+int main(){
+	long int n,i,num,cou=0;
+	cin>>n;
+	
+	for(i=1;i<=n;i++){
+		cin>>num;		
+		mp[i].push_back(num);
+		mp[num].push_back(i);		
+	}
+	
+	for(i=1;i<=n;i++){
+		
+		if(!visited[i]){
+			cou++;
+			dfs(i);
+		}
+	}
+	cout<<cou;
+	
+}
