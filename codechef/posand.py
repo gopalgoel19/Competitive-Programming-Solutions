cd0cc54544fd01099d777a440ebe7ba6f8902957
Ayushi <53288043+codeash14@users.noreply.github.com>
2020-10-20 22:11:53 +0530

    Added solution of October long 2020 (#77)
    * October long 2020 solution for covid run
    
    Added solution for:
    problem code- cvdrun
    contest code- oct20b
    
    * October long 2020 solution for chef and easy query
    
    added solution for:
    problem code- chefezq
    contest code- oct20b
    
    * October long 2020 solution for positive AND
    
    Added solution for:
    problem code- posand
    contest code- oct20b

---
+++ codechef/posand.py
@@ -0,0 +1,27 @@
+def pot(a):
+    return a!=0 and ((a&(a-1))==0)
+
+for _ in range(int(input())):
+    n=int(input())
+    if n==1:
+        print("1")
+    elif n==3:
+        print("1 3 2")
+    elif n==5:
+        print("2 3 1 5 4")
+    elif(pot(n)):
+        print(-1)
+    else:
+        print("2 3 1 5 4",end=" ")
+        i=6
+        while(i<=n):
+            if(pot(i)):
+                print(i+1,i,end=" ")
+                i+=2
+            else:
+                print(i,end=" ")
+                i+=1
+    print()
+        
+'''problem code- POSAND
+    contest code- OCT20B'''
